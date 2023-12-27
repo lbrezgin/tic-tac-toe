53b2120de82e407e4eb70996366b5fdb68dffa38
lewick <lewickbrez@gmail.com>
2023-12-27 16:36:38 +0200

    feat: 1.0.0

---
+++ tic_tac_toe/tic_tac_toe/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace tic_tac_toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butNewGame = new System.Windows.Forms.Button();
+            this.but2 = new System.Windows.Forms.Button();
+            this.but3 = new System.Windows.Forms.Button();
+            this.but4 = new System.Windows.Forms.Button();
+            this.but5 = new System.Windows.Forms.Button();
+            this.but6 = new System.Windows.Forms.Button();
+            this.but7 = new System.Windows.Forms.Button();
+            this.but8 = new System.Windows.Forms.Button();
+            this.but9 = new System.Windows.Forms.Button();
+            this.but1 = new System.Windows.Forms.Button();
+            this.butStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // butNewGame
+            // 
+            this.butNewGame.Location = new System.Drawing.Point(216, 488);
+            this.butNewGame.Name = "butNewGame";
+            this.butNewGame.Size = new System.Drawing.Size(151, 69);
+            this.butNewGame.TabIndex = 1;
+            this.butNewGame.Text = "New Game";
+            this.butNewGame.UseVisualStyleBackColor = true;
+            this.butNewGame.Visible = false;
+            this.butNewGame.Click += new System.EventHandler(this.butNewGame_Click);
+            // 
+            // but2
+            // 
+            this.but2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but2.Location = new System.Drawing.Point(226, 99);
+            this.but2.Name = "but2";
+            this.but2.Size = new System.Drawing.Size(130, 120);
+            this.but2.TabIndex = 3;
+            this.but2.UseVisualStyleBackColor = true;
+            this.but2.Click += new System.EventHandler(this.but2_Click);
+            // 
+            // but3
+            // 
+            this.but3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but3.Location = new System.Drawing.Point(362, 99);
+            this.but3.Name = "but3";
+            this.but3.Size = new System.Drawing.Size(130, 120);
+            this.but3.TabIndex = 5;
+            this.but3.UseVisualStyleBackColor = true;
+            this.but3.Click += new System.EventHandler(this.but3_Click);
+            // 
+            // but4
+            // 
+            this.but4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but4.Location = new System.Drawing.Point(90, 225);
+            this.but4.Name = "but4";
+            this.but4.Size = new System.Drawing.Size(130, 120);
+            this.but4.TabIndex = 6;
+            this.but4.UseVisualStyleBackColor = true;
+            this.but4.Click += new System.EventHandler(this.but4_Click);
+            // 
+            // but5
+            // 
+            this.but5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but5.Location = new System.Drawing.Point(226, 225);
+            this.but5.Name = "but5";
+            this.but5.Size = new System.Drawing.Size(130, 120);
+            this.but5.TabIndex = 7;
+            this.but5.UseVisualStyleBackColor = true;
+            this.but5.Click += new System.EventHandler(this.but5_Click);
+            // 
+            // but6
+            // 
+            this.but6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but6.Location = new System.Drawing.Point(362, 225);
+            this.but6.Name = "but6";
+            this.but6.Size = new System.Drawing.Size(130, 120);
+            this.but6.TabIndex = 8;
+            this.but6.UseVisualStyleBackColor = true;
+            this.but6.Click += new System.EventHandler(this.but6_Click);
+            // 
+            // but7
+            // 
+            this.but7.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but7.Location = new System.Drawing.Point(90, 351);
+            this.but7.Name = "but7";
+            this.but7.Size = new System.Drawing.Size(130, 120);
+            this.but7.TabIndex = 9;
+            this.but7.UseVisualStyleBackColor = true;
+            this.but7.Click += new System.EventHandler(this.but7_Click);
+            // 
+            // but8
+            // 
+            this.but8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but8.Location = new System.Drawing.Point(226, 351);
+            this.but8.Name = "but8";
+            this.but8.Size = new System.Drawing.Size(130, 120);
+            this.but8.TabIndex = 10;
+            this.but8.UseVisualStyleBackColor = true;
+            this.but8.Click += new System.EventHandler(this.but8_Click);
+            // 
+            // but9
+            // 
+            this.but9.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but9.Location = new System.Drawing.Point(362, 351);
+            this.but9.Name = "but9";
+            this.but9.Size = new System.Drawing.Size(130, 120);
+            this.but9.TabIndex = 11;
+            this.but9.UseVisualStyleBackColor = true;
+            this.but9.Click += new System.EventHandler(this.but9_Click);
+            // 
+            // but1
+            // 
+            this.but1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.but1.Location = new System.Drawing.Point(90, 99);
+            this.but1.Name = "but1";
+            this.but1.Size = new System.Drawing.Size(130, 120);
+            this.but1.TabIndex = 12;
+            this.but1.UseVisualStyleBackColor = true;
+            this.but1.Click += new System.EventHandler(this.but1_Click);
+            // 
+            // butStart
+            // 
+            this.butStart.Location = new System.Drawing.Point(216, 12);
+            this.butStart.Name = "butStart";
+            this.butStart.Size = new System.Drawing.Size(151, 69);
+            this.butStart.TabIndex = 13;
+            this.butStart.Text = "Start";
+            this.butStart.UseVisualStyleBackColor = true;
+            this.butStart.Click += new System.EventHandler(this.butStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 629);
+            this.Controls.Add(this.butStart);
+            this.Controls.Add(this.but1);
+            this.Controls.Add(this.but9);
+            this.Controls.Add(this.but8);
+            this.Controls.Add(this.but7);
+            this.Controls.Add(this.but6);
+            this.Controls.Add(this.but5);
+            this.Controls.Add(this.but4);
+            this.Controls.Add(this.but3);
+            this.Controls.Add(this.but2);
+            this.Controls.Add(this.butNewGame);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(600, 700);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(600, 700);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butNewGame;
+        private System.Windows.Forms.Button but2;
+        private System.Windows.Forms.Button but3;
+        private System.Windows.Forms.Button but4;
+        private System.Windows.Forms.Button but5;
+        private System.Windows.Forms.Button but6;
+        private System.Windows.Forms.Button but7;
+        private System.Windows.Forms.Button but8;
+        private System.Windows.Forms.Button but9;
+        private System.Windows.Forms.Button but1;
+        private System.Windows.Forms.Button butStart;
+    }
+}
+
---
+++ tic_tac_toe/tic_tac_toe/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tic_tac_toe
+{
+    public partial class Form1 : Form
+    {
+        int player = 1;
+        string[] roles = { "X", "O" };
+        bool win = false;
+        int moves = 9;
+        public Form1()
+        {
+            InitializeComponent();
+            EnableAll("disable");
+        }
+        private void EnableAll(string todo)
+        {
+            if (todo == "enable")
+            {
+                but1.Enabled = true;
+                but2.Enabled = true;
+                but3.Enabled = true;
+                but4.Enabled = true;
+                but5.Enabled = true;
+                but6.Enabled = true;
+                but7.Enabled = true;
+                but8.Enabled = true;
+                but9.Enabled = true;
+            }
+           else
+            {
+                if (todo == "disable")
+                {
+                    but1.Enabled = false;
+                    but2.Enabled = false;
+                    but3.Enabled = false;
+                    but4.Enabled = false;
+                    but5.Enabled = false;
+                    but6.Enabled = false;
+                    but7.Enabled = false;
+                    but8.Enabled = false;
+                    but9.Enabled = false;
+                }
+            }
+        }
+        private void Move(Button but)
+        {
+            if (player == 1)
+            {
+                but.Text = roles[0];
+                player = 2;
+            }
+            else
+            {
+                but.Text = roles[1];
+                player = 1;
+            }
+            moves--;
+        }
+
+        private void CheckWin(string[] array)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                if (but1.Text == array[i] && but2.Text == array[i] && but3.Text == array[i])
+                {
+                    Win(array[i]);
+                }
+                else
+                {
+                    if (but4.Text == array[i] && but5.Text == array[i] && but6.Text == array[i])
+                    {
+                        Win(array[i]);
+                    }
+                    else
+                    {
+                        if (but7.Text == array[i] && but8.Text == array[i] && but9.Text == array[i])
+                        {
+                            Win(array[i]);
+                        }
+                        else
+                        {
+                            if (but1.Text == array[i] && but5.Text == array[i] && but9.Text == array[i])
+                            {
+                                Win(array[i]);
+                            }
+                            else
+                            {
+                                if (but3.Text == array[i] && but5.Text == array[i] && but7.Text == array[i])
+                                {
+                                    Win(array[i]);
+                                }
+                                else
+                                {
+                                    if (but1.Text == array[i] && but4.Text == array[i] && but7.Text == array[i])
+                                    {
+                                        Win(array[i]);
+                                    }
+                                    else
+                                    {
+                                        if (but2.Text == array[i] && but5.Text == array[i] && but8.Text == array[i])
+                                        {
+                                            Win(array[i]);
+                                        }
+                                        else
+                                        {
+                                            if (but3.Text == array[i] && but6.Text == array[i] && but9.Text == array[i])
+                                            {
+                                                Win(array[i]);
+                                            }
+                                            else
+                                            {
+                                                if (moves == 0 && win == false)
+                                                {
+                                                    Draw();
+                                                    break;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } 
+        private void Draw()
+        {
+            EnableAll("disable");
+            butNewGame.Visible = true;
+            MessageBox.Show("Draw!");
+        }
+        private void Win(string who)
+        {
+            win = true;
+            EnableAll("disable");
+            butNewGame.Visible = true;
+            MessageBox.Show(who + " win!");
+
+        }
+
+        private void but1_Click(object sender, EventArgs e)
+        {
+            Move(but1);
+            CheckWin(roles);
+        }
+
+        private void but2_Click(object sender, EventArgs e)
+        {
+            Move(but2);
+            CheckWin(roles);
+        }
+
+        private void but3_Click(object sender, EventArgs e)
+        {
+            Move(but3);
+            CheckWin(roles);
+        }
+
+        private void but4_Click(object sender, EventArgs e)
+        {
+            Move(but4);
+            CheckWin(roles);
+        }
+
+        private void but5_Click(object sender, EventArgs e)
+        {
+            Move(but5);
+            CheckWin(roles);
+        }
+
+        private void but6_Click(object sender, EventArgs e)
+        {
+            Move(but6);
+            CheckWin(roles);
+        }
+
+        private void but7_Click(object sender, EventArgs e)
+        {
+            Move(but7);
+            CheckWin(roles);
+        }
+
+        private void but8_Click(object sender, EventArgs e)
+        {
+            Move(but8);
+            CheckWin(roles);
+        }
+
+        private void but9_Click(object sender, EventArgs e)
+        {
+           Move(but9);
+           CheckWin(roles);
+        }
+
+        private void butStart_Click(object sender, EventArgs e)
+        {
+            EnableAll("enable");
+            butStart.Visible = false;
+        }
+
+        private void butNewGame_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 newForm = new Form1();
+            newForm.ShowDialog();
+        }
+    }
+}
